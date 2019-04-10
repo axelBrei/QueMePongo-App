0ba0fb59d96ad2b3c055056e62fb3ea4e7ade1e0
abreiterman <axel.breiterman@valtech.com>
2019-04-10 15:55:05 -0300

    Commit inicial, proyecto vacio

---
+++ app/src/main/java/dds/frba/utn/quemepongo/LoginActivity.java
@@ -0,0 +1,13 @@
+package dds.frba.utn.quemepongo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
